b3f68491359099830880c61aa285d9ce4c920694
Filip R <framazan@davidsonacademy.unr.edu>
2021-11-12 14:16:48 -0800

    add some files and edit certain lines

--- launchers/default.sh
+++ launchers/default.sh
@@ -11,10 +11,11 @@ dt-launchfile-init
 
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
-
+roscore &
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
-
+#dt-exec echo "This is an empty launch script. Update it to launch your application."
+sleep 5
+dt-exec rosrun package_dts hello.py
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
